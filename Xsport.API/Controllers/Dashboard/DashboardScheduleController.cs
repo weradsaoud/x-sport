288afff5b78585abaae1ede5f2abbb078f4bd242
mohammad.mousa <mohammad.mousa@nasca-sy.com>
2024-05-30 17:13:44 +0300

    - add the owner of the stadium (the creator). - implement the dashboard schedule functionality.

---
+++ Xsport.API/Controllers/Dashboard/DashboardScheduleController.cs
@@ -0,0 +1,106 @@
+﻿using AutoWrapper.Wrappers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Xsport.Common.Emuns;
+using Xsport.Common.Enums;
+using Xsport.Common.Models;
+using Xsport.Core;
+using Xsport.Core.DashboardServices.UserServices;
+using Xsport.Core.ReservationServices;
+using Xsport.DB.Entities;
+using Xsport.DTOs.ReservationDtos;
+using Xsport.DTOs.StadiumDtos.DashboardDtos;
+using Xsport.DTOs.UserDtos;
+
+namespace Xsport.API.Controllers.Dashboard
+{
+    [ApiController]
+    [Route("api/dashboard/[controller]/[action]")]
+    [Tags("DashboardSchedule")]
+    [ApiExplorerSettings(GroupName = "dashboard")]
+    public class DashboardScheduleController : BaseController
+    {
+        private IDashboarScheduleServices _dashboarScheduleServices;
+        public DashboardScheduleController(IDashboarScheduleServices dashboarScheduleServices)
+        {
+            _dashboarScheduleServices = dashboarScheduleServices;
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost]
+        public async Task<List<DashboardDailyReservationSlotDto>> GetStadiumDailyReservationsSlots([FromBody] DailyReservationDto dto)
+        {
+                try
+                {
+                if (LoggedInUser == null) throw new ApiException("You are not signed in.");
+                return await _dashboarScheduleServices.GetStadiumFloorDailyReservationsSlots(dto, CurrentLanguageId);
+                }
+                catch (Exception ex)
+                {
+                    throw new ApiException(ex.Message, 500);
+                }
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost]
+        public async Task<List<DashboardDailyReservationDto>> GetStadiumDailyReservations([FromBody] DailyReservationDto dto)
+        {
+            try
+            {
+                if (LoggedInUser == null) throw new ApiException("You are not signed in.");
+                return await _dashboarScheduleServices.GetStadiumFloorDailyReservations(dto, CurrentLanguageId);
+            }
+            catch (Exception ex)
+            {
+                throw new ApiException(ex.Message, 500);
+            }
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost]
+        public async Task<List<DashboardMonthlyReservationDto>> GetStadiumMonthlyReservations([FromBody] MonthlyReservationDto dto)
+        {
+                try
+                {
+                if (LoggedInUser == null) throw new ApiException("You are not signed in.");
+                return await _dashboarScheduleServices.GetStadiumMonthlyReservations(dto, CurrentLanguageId);
+                }
+                catch (Exception ex)
+                {
+                    throw new ApiException(ex.Message, 500);
+                }
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet]
+        public async Task<OwnerStadiumDto> GetOwnerStadiums()
+        {
+            try
+            {
+                if (LoggedInUser == null) throw new ApiException("You are not signed in.");
+                return await _dashboarScheduleServices.GetOwnerStadiums(LoggedInUser.Id);
+            }
+            catch (Exception ex)
+            {
+                throw new ApiException(ex.Message, 500);
+            }
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet]
+        public async Task<StadiumFloorsDto> GetStadiumFloors(long stadiumId)
+        {
+            try
+            {
+                if (LoggedInUser == null) throw new ApiException("You are not signed in.");
+                return await _dashboarScheduleServices.GetStadiumFloors(stadiumId);
+            }
+            catch (Exception ex)
+            {
+                throw new ApiException(ex.Message, 500);
+            }
+        }
+
+    }
+}
---
+++ Xsport.Core/DashboardServices/ScheduleServices/IDashboarScheduleServices.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xsport.DTOs.ReservationDtos;
+using Xsport.DTOs.StadiumDtos;
+using Xsport.DTOs.StadiumDtos.DashboardDtos;
+
+namespace Xsport.Core.ReservationServices
+{
+    public interface IDashboarScheduleServices
+    {
+        Task<OwnerStadiumDto> GetOwnerStadiums(long ownerId);
+        Task<StadiumFloorsDto> GetStadiumFloors(long stadiumId);
+        Task<List<DashboardDailyReservationSlotDto>> GetStadiumFloorDailyReservationsSlots(DailyReservationDto dto, short currentLanguageId);
+        Task<List<DashboardDailyReservationDto>> GetStadiumFloorDailyReservations(DailyReservationDto dto, short currentLanguageId);
+        Task<List<DashboardMonthlyReservationDto>> GetStadiumMonthlyReservations(MonthlyReservationDto dto, short currentLanguageId);
+    }
+}
